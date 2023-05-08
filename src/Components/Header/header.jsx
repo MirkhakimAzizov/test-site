687b6f49f5825d8260c834af7950c6e532820aa1
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-08 23:46:50 +0500

    Update header.jsx

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -7,7 +7,7 @@ const header = () => {
                 <nav className="header-nav pt-3 bg-secondary bg-opacity-10 shadow shadow-sm">
                     <div className="container-fluid d-flex align-items-center justify-content-between">
                         <a className="bold" href="#">
-                            <strong>TEST.UZ</strong>
+                            <strong>FrontEdu.uz</strong>
                         </a>
                         <button className="login-btn btn btn-primary">
                             Kirish
